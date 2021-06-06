05d18baffed56a90f3a2efd374498bd3afeb6ce3
vishaalsharmaa <viky1441@gmail.com>
2021-06-06 21:39:54 +0530

    prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBTeBZoqWapcNjBRGivdgwsr17v-cZLFyU",
+    authDomain: "divss-birthday.firebaseapp.com",
+    databaseURL: "https://divss-birthday-default-rtdb.firebaseio.com",
+    projectId: "divss-birthday",
+    storageBucket: "divss-birthday.appspot.com",
+    messagingSenderId: "44962447223",
+    appId: "1:44962447223:web:d91c3443561e1bf3436b72",
+    measurementId: "G-JSV7D85BQF"
+  }
 };
